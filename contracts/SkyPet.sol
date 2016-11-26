f6be685a9b77e1c369f282b418b36613c2428fef
Daniel Stahl <danstahl1138@gmail.com>
2016-11-25 20:44:19 -0600

    working unit tests

--- contracts/SkyPet.sol
+++ contracts/SkyPet.sol
@@ -13,7 +13,6 @@ contract SkyPet{
     address public owner;
     modifier onlyOwner { if (msg.sender == owner) _; } //ensure only owner does some things
     modifier onlyIfHasEnoughEther { if (msg.value>=costToAdd) _; }  
-    //modifier hasError(bool noError) {if (!noError)_;}
     struct Attribute{
       uint timestamp;
       string jsonText; //must be a json string...if not, wont decrypt in client, and your loss
@@ -21,14 +20,14 @@ contract SkyPet{
     function SkyPet(){ //owner is creator of contract
       owner=msg.sender;
     }
-    mapping(bytes32=> Attribute[]) public pet; // hash of pet id to array of attributes
+    mapping(bytes32=> Attribute[]) private pet; // hash of pet id to array of attributes
     event attributeAdded(bytes32 _petid, string _attribute);
-    function checkSendFunds(bool hasError){
+    function checkSendFunds(bool hasError) private{
       if(hasError){
         throw;
       }
     }
-    function addAttribute(bytes32 _petid, string _attribute) onlyIfHasEnoughEther{//see https://blog.ethcore.io/condition-oriented-programming-2/
+    function addAttribute(bytes32 _petid, string _attribute) onlyIfHasEnoughEther payable {//see https://blog.ethcore.io/condition-oriented-programming-2/
       checkSendFunds(!msg.sender.send(msg.value-costToAdd));//guaranteed to be at least 0
       pet[_petid].push(Attribute(now, _attribute));
       attributeAdded(_petid, _attribute); //alert watchers that transaction went through
@@ -36,14 +35,18 @@ contract SkyPet{
     function kill() onlyOwner{
       selfdestruct(owner); // Makes contract inactive, returns funds
     }
-    function getNumberOfAttributes(bytes32 _petid) returns (uint){
+    function getNumberOfAttributes(bytes32 _petid) public constant returns (uint){
       return pet[_petid].length;
+    }
+    function getAttribute(bytes32 _petid, uint index) public constant returns(uint, string){
+      //return(pet[_petid][index].timesamp);
+      return(pet[_petid][index].timestamp, pet[_petid][index].jsonText);
     }
     function () {
         throw; // throw reverts state to before call
     }
     
-    function getRevenue() onlyOwner{ //scrape currently obtained revenue.  Dont do this every transaction to save on transaction costs
+    function getRevenue() onlyOwner payable{ //scrape currently obtained revenue.  Dont do this every transaction to save on transaction costs
       checkSendFunds(!owner.send(this.balance));
     }
 }
--- test/SkyPet.js
+++ test/SkyPet.js
@@ -1,34 +1,37 @@
 contract('SkyPet', function(accounts) {
-  //console.log(web3);
-  //console.log(web3.eth.getBalance(accounts[0]).toNumber());
-  //console.log(accounts[0].getBalance());
+  var testSha=web3.sha3("SomePetId");
   it("should give the cost to add", function() {
     var skypet = SkyPet.deployed();
     return skypet.costToAdd().then((cost)=>{
       assert.equal(cost, 100000000000000000, "Cost to add incorrect");
     });
-    
+  });
+  it("account has enough funds", function() {
+    var skypet = SkyPet.deployed();
+    return skypet.costToAdd().then((cost)=>{
+      assert(cost<web3.eth.getBalance(accounts[0]), "Not enough funds!");
+    });
   });
   it("should create a new item on ethereum", function() {
     // Get a reference to the deployed SkyPet contract, as a JS object.
     var skypet = SkyPet.deployed();
-    return skypet.addAttribute.call("SomePetId",  "helloworld").then((balance)=>{
-      //console.log(balance);
-      return skypet.getNumberOfAttributes("SomePetId").then((result)=>{
-        console.log(result);
-        //console.log(nr);
-        assert.equal(result, 1, "account not added");
+    return skypet.costToAdd().then((cost)=>{
+      return skypet.addAttribute.sendTransaction(testSha, "helloworld", {value:cost, gas:3000000}).then((newIndex)=>{
+        return skypet.getAttribute(testSha, 0).then((result)=>{
+          assert.equal(result[1], "helloworld", "account not added");
+        });
       });
-      
     });
   });
-  /*it("should fail from lack of funds", function() {
-    // Get a reference to the deployed MetaCoin contract, as a JS object.
+  it("should get number of items", function() {
+    // Get a reference to the deployed SkyPet contract, as a JS object.
     var skypet = SkyPet.deployed();
-
-    // Get the MetaCoin balance of the first account and assert that it's 10000.
-    return skypet.addAttribute.call("SomePetId",  "helloworld").then(function(balance) {
-      assert.equal(skypet.getNumberOfAttributes("SomePetId"), 1, "account not added");
+    return skypet.costToAdd().then((cost)=>{
+      return skypet.addAttribute.sendTransaction(testSha, "helloworld", {value:cost, gas:3000000}).then((newIndex)=>{
+        return skypet.getNumberOfAttributes(testSha).then((result)=>{
+          assert.equal(result, 2, "attributes not working");
+        });        
+      });
     });
-  });*/
+  });
 });
