b8bb332030edc8e4d224910e5cbb9d0c888c2b1f
Daniel Stahl <danstahl1138@gmail.com>
2016-11-27 10:05:16 -0600

    changed address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 module.exports = function(deployer) {
-  deployer.deploy(SkyPet, '0x69De4ADbb566c1c68e8dB1274229adA4A3D9f8A8');
+  deployer.deploy(SkyPet, '0x72c1bba9cabab4040c285159c8ea98fd36372858');
+  //actually 0x72c1bba9cabab4040c285159c8ea98fd36372858?
   deployer.autolink();
   //deployer.deploy(MetaCoin);
 };
